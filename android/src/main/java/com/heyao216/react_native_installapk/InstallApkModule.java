71375b1ab463af61f0e4fbf88a834ee9a54363c7
Emidio Croci <ecroci@kseniasecurity.com>
2018-09-06 15:57:25 +0200

    Added silentInstall

--- android/src/main/java/com/heyao216/react_native_installapk/InstallApkModule.java
+++ android/src/main/java/com/heyao216/react_native_installapk/InstallApkModule.java
@@ -1,17 +1,24 @@
 package com.heyao216.react_native_installapk;
 
+import android.app.PendingIntent;
+import android.content.IntentSender;
+import android.content.pm.PackageInstaller;
 import android.os.Build;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v4.content.FileProvider;
 
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
 /**
  * Created by heyao on 2016/11/4.
@@ -31,7 +38,7 @@ public class InstallApkModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void install(String path, String provider) {
-        String cmd = "chmod 777 " +path;
+        String cmd = "chmod 777 " + path;
         try {
             Runtime.getRuntime().exec(cmd);
         } catch (Exception e) {
@@ -41,13 +48,58 @@ public class InstallApkModule extends ReactContextBaseJavaModule {
         Intent intent = new Intent(Intent.ACTION_VIEW);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         Uri uri;
-        if(Build.VERSION.SDK_INT >= 24) {
-          uri = FileProvider.getUriForFile(_context, provider, apkFile);
-          intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-        } else{
-          uri = Uri.fromFile(apkFile);
+        if (Build.VERSION.SDK_INT >= 24) {
+            uri = FileProvider.getUriForFile(_context, provider, apkFile);
+            intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        } else {
+            uri = Uri.fromFile(apkFile);
         }
         intent.setDataAndType(uri, "application/vnd.android.package-archive");
         _context.startActivity(intent);
+    }
+
+    @ReactMethod
+    public void silentInstall(String path, String packageName, Promise promise) {
+        if (android.os.Build.VERSION.SDK_INT >= 21) {
+            try {
+                File file = new File(path);
+                FileInputStream inputStream = new FileInputStream(file);
+                PackageInstaller packageInstaller = _context.getPackageManager().getPackageInstaller();
+                int sessionId = 0;
+                sessionId = packageInstaller.createSession(new PackageInstaller
+                        .SessionParams(PackageInstaller.SessionParams.MODE_FULL_INSTALL));
+                PackageInstaller.Session session = packageInstaller.openSession(sessionId);
+                long sizeBytes = 0;
+
+                OutputStream out = null;
+                out = session.openWrite("my_app_session", 0, sizeBytes);
+
+                int total = 0;
+                byte[] buffer = new byte[65536];
+                int c;
+                while ((c = inputStream.read(buffer)) != -1) {
+                    total += c;
+                    out.write(buffer, 0, c);
+                }
+                session.fsync(out);
+                inputStream.close();
+                out.close();
+
+                session.commit(createIntentSender(sessionId));
+
+                promise.resolve(true);
+            } catch (IOException e) {
+                promise.reject("error", e);
+            }
+        }
+    }
+
+    private IntentSender createIntentSender(int sessionId) {
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(
+                _context,
+                sessionId,
+                new Intent(Intent.ACTION_MY_PACKAGE_REPLACED),
+                0);
+        return pendingIntent.getIntentSender();
     }
 }
--- android/src/main/java/com/heyao216/react_native_installapk/InstallApkPackager.java
+++ android/src/main/java/com/heyao216/react_native_installapk/InstallApkPackager.java
@@ -20,6 +20,11 @@ public class InstallApkPackager implements ReactPackage{
     }
 
     @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
+    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
     }
